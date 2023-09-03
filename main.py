3dbad6b707cea0b62f5be40c6086ebc4184924d7
allexkap <aekpstn@gmail.com>
2023-09-03 23:58:49 +0300

    feat inactive time

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import win32api
 import win32gui
 import win32process
 import psutil
 
+
+def get_idle_sec():
+    return win32api.GetTickCount() - win32api.GetLastInputInfo()
+
 def getProcessName():
+    if get_idle_sec > 30_000:
+        return None
     hwnd = win32gui.GetForegroundWindow()
     _, pid = win32process.GetWindowThreadProcessId(hwnd)
     name = psutil.Process(pid).name()

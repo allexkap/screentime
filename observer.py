93a162e8ae19e815c19dd0aa3e7fbbc8f4bab769
allexkap <aekpstn@gmail.com>
2023-09-05 12:49:31 +0300

    add dataclass

--- observer.py
+++ observer.py
@@ -2,16 +2,25 @@ import os
 import time
 import yaml
 import asyncio
+from dataclasses import dataclass, field
 
 
-async def wait(delay, coro):
-    await asyncio.sleep(delay)
-    await coro
-
-async def repeat(func, delay):
+async def repeat(func, delay, wait=0):
+    if wait:
+        await asyncio.sleep(wait)
     while True:
         func()
         await asyncio.sleep(delay)
+
+def until00min():
+    now = time.localtime()
+    return (60 - now.tm_min) * 60 + now.tm_sec
+
+
+@dataclass
+class Cache:
+    path: str
+    score: dict[str, int] = field(default_factory=dict)
 
 
 class Observer:
@@ -21,32 +30,33 @@ class Observer:
         self.path = path
         self.weight = weight
         self.pattern = pattern
-        self.cache = self.load_cache()
+        self.cache = self.loadCache()
 
-    def load_cache(self):
+    def loadCache(self):
         path = f'{self.path}/{time.strftime(self.pattern, time.localtime())}'
-        if os.path.exists(path):
-            with open(path) as file:
-                return yaml.load(file.read(), yaml.Loader)
-        return {'path': path, 'points': dict()}
+        if not os.path.exists(path):
+            return Cache(path)
+        with open(path) as file:
+            return Cache(path, yaml.load(file.read(), yaml.Loader))
 
     def reload(self):
         self.commit()
-        self.cache = load_cache()
+        self.cache = self.loadCache()
 
     def update(self):
         window = self.func()
-        if window not in self.cache['points']:
-            self.cache['points'][window] = 0
-        self.cache['points'][window] += self.weight
+        if window not in self.cache.score:
+            self.cache.score[window] = 0
+        self.cache.score[window] += self.weight
 
     def commit(self):
-        with open(self.cache['path'], 'w') as file:
-            file.write(yaml.dump(self.cache['points']))
+        with open(self.cache.path, 'w') as file:
+            file.write(yaml.dump(self.cache.score))
 
     async def observe(self):
-        tasks = (
+        coros = (
             repeat(self.update, delay=1),
             repeat(self.commit, delay=10*60),
-            repeat(self.reload, delay=60*60, wait=(60-r.tm_min)*60+r.tm_sec)
+            repeat(self.reload, delay=60*60, wait=until00min())
         )
+        await asyncio.gather(*coros)

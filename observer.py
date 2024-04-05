ac0fb05f686df0969f0c7dbe424ebf84279245e8
allexkap <aekpstn@gmail.com>
2024-04-05 23:47:57 +0300

    migrate to db

--- observer.py
+++ observer.py
@@ -1,84 +1,103 @@
 import asyncio
-import os
-from dataclasses import dataclass, field
+import logging
+from argparse import ArgumentParser
 from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Callable, NoReturn
 
-import yaml
+import psutil
+import win32api
+import win32gui
+import win32process
+
+from db import Activity
+
+logging.basicConfig(
+    level=logging.WARNING,
+    format='[%(asctime)s] %(levelname)s %(filename)s:%(lineno)d %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=(
+        logging.FileHandler(Path(__file__).with_suffix('.log'), 'a'),
+        logging.StreamHandler(),
+    ),
+)
 
 
-async def lightsleep(seconds):
+def safe(default, attempts=3):
+    def dec(func):
+        def inner(*args, **kwargs):
+            for _ in range(attempts):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as ex:
+                    logging.warning(ex)
+            return default
+
+        return inner
+
+    return dec
+
+
+async def lightsleep(seconds: float) -> None:
     date = datetime.now() + timedelta(seconds=seconds)
     while datetime.now() < date:
         await asyncio.sleep(1)
 
 
-async def repeat(func, arg):
-    res = None
+async def repeat(func: Callable, seconds: float, now=False) -> NoReturn:
+    delta = seconds if not now else 0
     while True:
-        await lightsleep(res or (arg() if callable(arg) else arg))
-        res = func()
+        await lightsleep(delta)
+        delta = func()
+        if delta is None:
+            delta = seconds
 
 
-@dataclass
-class Cache:
-    path: str
-    timestamp: float
-    score: dict[str, int] = field(default_factory=dict)
+@safe(default=0, attempts=1)
+def get_idle_sec() -> int:
+    return win32api.GetTickCount() - win32api.GetLastInputInfo()
 
 
-class Observer:
-    def __init__(self, watch, idle, path='.', intervals={}, pattern=r'%y%m%d%H'):
-        self.watch = watch
-        self.idle = idle
-        self.path = path
-        self.pattern = pattern
-        self.intervals = {
-            'update': 1,
-            'commit': 60,
-            'idle': 60,
-        }
-        self.intervals.update(intervals)
-        self.cache = self.loadCache()
-        self.is_active = True
+@safe(default='?', attempts=1)
+def get_process_name() -> str:
+    hwnd = win32gui.GetForegroundWindow()
+    _, pid = win32process.GetWindowThreadProcessId(hwnd)
+    name = psutil.Process(pid).name()
+    return name
 
-    def loadCache(self):
-        timestamp = datetime.now()
-        ts = timestamp.timestamp() // 3600
-        path = f'{self.path}/{timestamp.strftime(self.pattern)}'
-        if not os.path.exists(path):
-            return Cache(path, ts)
-        with open(path) as file:
-            return Cache(path, ts, yaml.load(file.read(), yaml.Loader))
 
-    def validateCache(self):
-        if datetime.now().timestamp() // 3600 != self.cache.timestamp:
-            self.commit()
-            self.cache = self.loadCache()
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        '-d',
+        '--db_path',
+        help='path to sqlite3 database',
+        default='./db.sqlite3',
+        type=Path,
+    )
+    return parser.parse_args()
 
-    def update(self):
-        self.validateCache()
-        if not self.is_active:
-            return
-        window = self.watch()
-        if window not in self.cache.score:
-            self.cache.score[window] = 0
-        self.cache.score[window] += self.intervals['update']
 
-    def commit(self):
-        with open(self.cache.path, 'w') as file:
-            file.write(yaml.dump(self.cache.score))
+def update():
+    app = get_process_name()
+    activity.update(app, 1)
 
-    def checkActivity(self):
-        sec = self.idle()
-        if sec < self.intervals['idle']:
-            self.is_active = True
-            return self.intervals['idle'] - sec
-        self.is_active = False
 
-    async def observe(self):
-        coros = (
-            repeat(self.update, arg=self.intervals['update']),
-            repeat(self.commit, arg=self.intervals['commit']),
-            repeat(self.checkActivity, arg=1),
-        )
-        await asyncio.gather(*coros)
+def commit():
+    activity.commit()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    activity = Activity(args.db_path)
+
+    loop = asyncio.new_event_loop()
+    try:
+        loop.create_task(repeat(update, 1))
+        loop.create_task(repeat(commit, 60))
+        loop.run_forever()
+    except Exception as ex:
+        logging.critical(ex)
+    finally:
+        loop.close()
